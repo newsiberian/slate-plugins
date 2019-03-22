615990a1bf055c0ac8780b56993560e72b322f46
Denis Akiyakov <newsiberian2015@yandex.ru>
2019-03-22 12:23:40 +0700

    - [storybook]: improved linkify default story; - bump deps;

--- stories/slate-linkify/default/index.js
+++ stories/slate-linkify/default/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Value } from 'slate';
 import { Editor } from 'slate-react';
 
+import { Button, Toolbar } from '../../components';
 import { linkifyPlugin } from '../../../packages/slate-linkify/lib';
 
 const plugins = [linkifyPlugin()];
@@ -27,6 +28,45 @@ const initialValue = Value.fromJSON({
   },
 });
 
+const getUrl = (editor) => {
+  const link = editor.value.inlines.filter(inline => inline.type === 'link').first();
+  return link.data.get('url');
+};
+
+const renderEditor = (props, editor, next) => {
+  const children = next();
+
+  /**
+   * Check if the any of the currently selected inlines are of `type`.
+   * @param {String} type
+   * @return {Boolean}
+   */
+  const hasInline = (type) => {
+    return editor.value.inlines.some(node => node.type === type);
+  };
+
+  function onAddEditLink(event) {
+    event.preventDefault();
+
+    const type = 'link';
+    const isActive = hasInline(type);
+    const oldUrl = isActive ? getUrl(editor) : undefined;
+    const url = window.prompt(isActive ? 'Edit link' : 'Create a link', oldUrl)
+    editor.command('wrapLink', url);
+  }
+
+  return (
+    <>
+      <Toolbar>
+        <Button onMouseDown={onAddEditLink}>
+          Add/Edit link
+        </Button>
+      </Toolbar>
+
+      {children}
+    </>
+  );
+};
 
 export default function LinkifyDefault({ readOnly = false }) {
   const [value, setValue] = useState(initialValue);
@@ -42,6 +82,7 @@ export default function LinkifyDefault({ readOnly = false }) {
         onChange={onChange}
         plugins={plugins}
         readOnly={readOnly}
+        renderEditor={renderEditor}
       />
     </section>
   );
