f463543675125662cb5e6570d4e0ec4d559609eb
Denis Akiyakov <newsiberian@users.noreply.github.com>
2023-06-03 13:52:47 +0400

    chore(slate-gallery-common): move to bundling by pure TSC

--- packages/slate-gallery-common/rollup.config.mjs
+++
@@ -1,4 +0,0 @@
-import { config } from '../../rollup.config.mjs';
-import packages from './package.json' assert { type: "json" };
-
-export default config('slate-gallery-common', Object.keys(packages.dependencies));
