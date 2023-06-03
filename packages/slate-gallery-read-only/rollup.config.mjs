b310c8dac14cb66272d158cd58878b091a0b0814
Denis Akiyakov <newsiberian@users.noreply.github.com>
2023-06-03 13:56:28 +0400

    chore(slate-gallery-read-only): move bundling to pure TSC

--- packages/slate-gallery-read-only/rollup.config.mjs
+++
@@ -1,4 +0,0 @@
-import { config } from '../../rollup.config.mjs';
-import packages from './package.json' assert { type: "json" };
-
-export default config('slate-gallery-read-only', Object.keys(packages.dependencies));
