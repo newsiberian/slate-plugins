7ab58a757f82fef7db24761c81df585c98afcb46
Denis Akiyakov <newsiberian@users.noreply.github.com>
2023-06-03 13:41:55 +0400

    chore(slate-video): change bundling to use TSC only w/o Rollup. I don't think someone needs UMD nowadays

--- packages/slate-video/rollup.config.mjs
+++
@@ -1,4 +0,0 @@
-import { config } from '../../rollup.config.mjs';
-import packages from './package.json' assert { type: "json" };
-
-export default config('slate-video', Object.keys(packages.dependencies));
